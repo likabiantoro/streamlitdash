51afad1994cbabd021e06e8bec8c6ba3026ffd70
likabiantoro <likabiantoro@gmail.com>
2024-12-11 07:47:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,46 @@
+# Install requirement
 import streamlit as st
 from datetime import time, datetime
 
+# Membuat slider dengan fungsi st.slider
 st.header('st.slider')
 
-# Example 1
+# Slider satu nilai
+# Menampilkan sub header 'Slider satu nilai'
+st.subheader('Slider satu nilai')
 
-st.subheader('Slider')
+# Memberi label pertanyaan 'Berapa usiamu saat ini'
+# 0 adalah nilai min, 100 adalah nilai max, dan 13 adalah titik awal slider
+age = st.slider('Berapa usiamu saat ini?', 0, 100, 13)
+st.write("Usia saya", age, 'tahun')
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+# Slider rentang nilai
+# Menampilkan sub header 'Slider rentang nilai'
+st.subheader('Slider rentang nilai')
 
-# Example 2
-
-st.subheader('Range slider')
-
+#  50adalah nilai min, 200 adalah nilai max, dan 150-170 adalah titik awal slider
 values = st.slider(
-     'Select a range of values',
-     0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
+     'Berapa rentang tinggi badan anda?',
+     50, 200, (150, 170)) 
+st.write('Tinggi badan saya adalah', values, 'cm')
 
-# Example 3
+# Slider rentang waktu
+# Menampilkan sub header 'Slider rentang waktu'
+st.subheader('Slider rentang waktu')
 
-st.subheader('Range time slider')
+# Rentang default waktu adalah 1:00 hingga 5:45
+sleeptime = st.slider(
+     "Pada pukul berapa anda tidur?",
+     value=(time(1, 00), time(5, 45)))
+st.write("Saya tidur pada pukul:", sleeptime)
 
-appointment = st.slider(
-     "Schedule your appointment:",
-     value=(time(11, 30), time(12, 45)))
-st.write("You're scheduled for:", appointment)
+# Slider tanggal dan waktu
+# Menampilkan sub header 'Slider rentang tanggal dan waktu'
+st.subheader('Slider tanggal dan waktu')
 
-# Example 4
-
-st.subheader('Datetime slider')
-
+# Default tanggal adalah 1 Januari 2024 dan Default waktu adalah 7:30 
 start_time = st.slider(
-     "When do you start?",
-     value=datetime(2020, 1, 1, 9, 30),
+     "Kapan anda berencana untuk melakukan konsultasi ke klinik Mitra Sehat?",
+     value=datetime(2024, 1, 1, 7, 30),
      format="MM/DD/YY - hh:mm")
-st.write("Start time:", start_time)
+st.write("Saya akan berkonsultasi pada", start_time)
