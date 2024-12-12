4f9942d46406c9386aeb757c886aae31c081d66a
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:24:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.header('st.multiselect')
-
+# opsi yang dapat dipilih
 options = st.multiselect(
      'Apa hewan peliharaan yang anda miliki',
      ['Kucing', 'Anjing', 'Kelinci', 'Ayam'],
-     ['Kucing', 'Anjing', 'Kelinci', 'Ayam'])
+     ['Kucing', 'Anjing']) # tampilan default
 
 st.write('Hewan peliharaan yang saya miliki adalah ', options)
