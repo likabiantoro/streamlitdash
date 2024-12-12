54004020bb5d96f3957532ac920756cd74afa4e4
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:53:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,12 @@ textColor="#FFFFFF"
 font="monospace"
 """)
 
+[theme]
+primaryColor="#F39C12"
+backgroundColor="#2E86C1"
+secondaryBackgroundColor="#AED6F1"
+textColor="#FFFFFF"
+font="monospace"
+
 number = st.sidebar.slider('Select a number:', 0, 10, 5)
 st.write('Selected number from slider widget is:', number)
