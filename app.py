89b8ed28ffa9e63d8ddfcc51e964cff27eff01d6
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:39:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,5 @@ st.latex(r'c^2 = a^2 + b^2')
 st.subheader('Rumus Relativitas Einstein')
 st.latex(r'E = mc^2')
 
-st.subheader('Persamaan Maxwell (Divergensi Medan Listrik)')
-st.latex(r'\nabla \cdot \mathbf{E} = \frac{\rho}{\varepsilon_0}')
-
-st.subheader('Persamaan Maxwell (Divergensi Medan Magnet)')
-st.latex(r'\nabla \cdot \mathbf{B} = 0')
+st.subheader('Persamaan Schrödinger')
+st.latex(r'\hat{H} \psi = E \psi')
