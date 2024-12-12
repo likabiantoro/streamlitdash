be4352496189532ca2add8e728fad63a4b15c804
likabiantoro <likabiantoro@gmail.com>
2024-12-11 21:31:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('Line chart')
+# Header untuk Line Chart
+st.header('Line Chart')
 
+# Data untuk Line Chart
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
+# Menampilkan Line Chart
 st.line_chart(chart_data)
+
+# Header untuk Bar Chart
+st.header('Bar Chart')
+
+# Data untuk Bar Chart
+bar_chart_data = pd.DataFrame(
+     np.random.randn(10, 3),
+     columns=['x', 'y', 'z'])
+
+# Menampilkan Bar Chart
+st.bar_chart(bar_chart_data)
