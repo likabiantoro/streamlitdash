0bf5f9ae9f18b7288cc81ba02ba608625544288e
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:15:00 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.header('st.selectbox')
 
 option = st.selectbox(
      'Apa hewan peliharaan yang anda miliki?',
-     ('Kucing', 'Anjing', 'Kelinci, 'Ayam))
+     ('Kucing', 'Anjing', 'Kelinci, 'Ayam'))
 
 st.write('Hewan peliharaan yang saya miliki adalah ', option)
