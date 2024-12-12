80643015e2838b978e5fbbbcda2f91c50fcad0c4
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:12:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-# Header untuk Line Chart
-st.header('Line Chart')
+st.header('st.selectbox')
 
-# Data untuk Line Chart
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
 
-# Menampilkan Line Chart
-st.line_chart(chart_data)
-
-# Spacer untuk pemisahan visual
-st.write("---")
-
-# Header untuk Bar Chart
-st.header('Bar Chart')
-
-# Data untuk Bar Chart
-bar_chart_data = pd.DataFrame(
-     np.random.randn(10, 3),
-     columns=['x', 'y', 'z'])
-
-# Menampilkan Bar Chart
-st.bar_chart(bar_chart_data)
+st.write('Your favorite color is ', option)
