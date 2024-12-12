39456298ef1eadf775da9233488e6b4ee67e2360
likabiantoro <likabiantoro@gmail.com>
2024-12-11 22:42:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ st.latex(r'c^2 = a^2 + b^2')
 st.subheader('Rumus Relativitas Einstein')
 st.latex(r'E = mc^2')
 
-st.subheader('Persamaan Schrödinger')
-st.latex(r'\hat{H} \psi = E \psi')
+st.subheader('Persamaan Gelombang')
+st.latex(r'\frac{\partial^2 u}{\partial t^2} = c^2 \nabla^2 u')
